f868112d9a0be73bd8cbf4ef49e2b264f96c8cf6
Andrea Epifani <andrea@andreaepifani.com>
2015-02-25 22:11:14 +1000

    Added separator on side bar. Added Font Awesome :)

--- views/_sidebar.php
+++ views/_sidebar.php
@@ -5,6 +5,8 @@
                     <a href="/new-blog" class="btn btn-square btn-block btn-lg btn-success">
                     <div class='square-content'><div><span><i class="fa fa-upload"></i><br>Submit</span></div></div>
                     </a><?php }?>
+                    <div class="clear"></div>
+                    <hr>
                     <h2>Weeks</h2>
                     <div class="panel-group" id="accordion">
 
